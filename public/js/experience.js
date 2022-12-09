c9d8a78dc55742b1ac0b8b354b37662c1f85ab13
gabrielrheaume <gabriel.rheaume1@hotmail.com>
2022-12-09 17:57:32 -0500

    almost done with homepage

--- public/js/experience.js
+++ public/js/experience.js
@@ -1,10 +1,22 @@
-const bloc_experience = document.querySelector(".experience")
+/* set dimension on text area for heading image */
+setTimeout(changeHeight, 0.5)
+
+function changeHeight()
+{
+    const heading_image = document.querySelector(".heading img")
+    height = heading_image.height
+    const text_area = document.querySelector(".heading .text-area")
+    text_area.style.height = height * 0.65 + "px"
+}
+
+/* set experience text the same height as the images behind */
+const experience_block = document.querySelector("section.experience")
 const img = document.querySelector(".experience .exp-container img")
-const bloc_texte = document.querySelector(".bloc-texte")
+const text_block = document.querySelector(".experience .bloc-texte")
 
 let height = img.height
 let new_height = height + 100
 
-bloc_experience.style.height = new_height + "px"
+experience_block.style.height = new_height + "px"
 img.style.height = height + "px"
-bloc_texte.style.height = height + "px"
+text_block.style.height = height + "px"
--- views/homepage.view.php
+++ views/homepage.view.php
@@ -6,8 +6,40 @@
     //$comments
 ?>
 
-<a href="menu">Menu</a>
+<!-- Heading Image -->
+<section class="heading">
+    <img src="public/images/calamar.jpg" alt="Présentation d'une longue assiette de calamar avec un petit pot de sauce rouge où des céleris se baignent." srcset="">
+    <div class="text-area">
+        <div class="titles">
+            <h1>PUB G4</h1>
+            <h4 class="slogan">Si réel, mais <br>pourtant surréaliste<br></h4>
+        </div>
+        <div class="text">
+            <p>Des plats succulents et variés vous attendent !</p>
+        </div>
+    </div>
+</section>
 
+<!-- Try Us Section -->
+<section class="try-us">
+    <div class="bloc-texte">
+        <h2>Testez notre cuisine !</h2>
+        <p>Maudit que ça l'air bon ! <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio expedita molestias totam minus aut amet consectetur eum ut! Tempora iure nemo dolore repellendus officia ut culpa. Ea asperiores adipisci omnis!
+        Nostrum cum maiores praesentium rerum, eius unde repudiandae alias a autem ad ex fuga neque quia porro blanditiis officia modi. Repellendus corrupti, reprehenderit optio doloribus maxime quos error molestias cumque.
+        Error unde doloremque beatae, autem consectetur aperiam fugiat consequuntur exercitationem veniam nemo porro similique nulla. Porro dolorem eos nam doloribus aspernatur et nostrum sequi tenetur, quisquam quae natus veritatis dolorum.
+        Nemo iusto dolorum corrupti asperiores.</p>
+        <a href="menu">Notre Menu</a>
+    </div>
+
+    <div class="images">
+        <div class="img1" alt="Un potage succulent !"></div>
+        <div class="img2" alt="Un burger bien gras avec ses frites"></div>
+        <div class="img3" alt="Un carré de brownie, servi avec son chapeau de crème glacée à la vanille transpercé d'un biscuit roulé, le tout arrosé d'un filet de chocolat et de caramel"></div>
+        <div class="img4" alt="Une salade césar qui fera le bonheur des plus herboristes d'entre nous"></div>
+    </div>
+</section>
+
+<!-- Experience Section -->
 <section class="experience">
     <div class="exp-container">
         <img src="public/images/experience.png" alt="Un alléchant tartare aux légumes au centre de son assiette accompagné d'un gateau au fromage avec coulis de caramel">
