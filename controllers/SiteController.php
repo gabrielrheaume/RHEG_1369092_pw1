76d20be4dd184c63952acf2bf3ac0a970b97f5c0
gabrielrheaume <gabriel.rheaume1@hotmail.com>
2022-12-13 15:48:02 -0500

    c cô lô

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -117,11 +117,11 @@
          */
         public function displayFormPage(string $title, string $display)
         {
-            // var_dump($display); exit();
             if($display == 'infolettre') $form_title = "S'inscrire à l'infolettre";
             if($display == 'connexion') $form_title = "Connexion";
             if($display == 'compte') $form_title = "Créer un compte";
             if($display == 'categorie') $form_title = "Modification de catégories";
+            if($display == 'menu') $form_title = "Modification du Menu";
             include("views/form.view.php");
         }
         
