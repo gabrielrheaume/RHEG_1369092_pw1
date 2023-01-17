dbdda37b9671a807a3eba7dbf4c0595ee6710ee5
Gabriel Rhéaume <gabriel.rheaume1@hotmail.com>
2023-01-17 15:09:44 -0500

    Fixed a huge security problem

--- config/database.php
+++ config/database.php
@@ -6,10 +6,4 @@
     $password = "";
     $dbname = "pw1_pubg4";
 
-    // Online site
-    // $host = "localhost";
-    // $username = "fc1369092_pw1";
-    // $password = "Ni,k5d@447&D";
-    // $dbname = "fc1369092_pw1";
-    
 ?>
